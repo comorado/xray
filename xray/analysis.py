24024692da348dee1c55cdecf18502041801fa88
Brian Mattern <rephorm@rephorm.com>
2012-09-02 20:45:32 -0700

    support generic operators (+,-,/,*)
    plus savitzky golay and a few utility routines

--- xray/analysis.py
+++ xray/analysis.py
@@ -1,9 +1,13 @@
 import numpy as np
+import operator
 
 class Curve(object):
   def __init__(self, x, y, s=None, **kwargs):
-    self.x = x
-    self.y = y
+    self.x = np.asarray(x)
+    self.y = np.asarray(y)
+
+    if s is not None:
+      s = np.asarray(s)
     self.s = s
 
     self.meta = kwargs
@@ -60,6 +64,31 @@ class Curve(object):
 
     return cls(x,y,s)
 
+  def copy(self):
+    return Curve(self.x.copy(), self.y.copy(), self.s.copy() if self.s else None, **self.meta)
+
+  def plot(self, **kwargs):
+    from matplotlib import pyplot as pt
+
+    axes = kwargs.pop('axes', None)
+    if axes is None: axes = pt.gca()
+    errorbars = kwargs.pop('errorbars', False)
+
+    if errorbars:
+      return axes.errorbar(self.x, self.y, self.s, **kwargs)
+    else:
+      return axes.plot(self.x, self.y, **kwargs)
+
+  def save(self, filename):
+    if self.s:
+      data = np.c_[self.x, self.y, self.s]
+    else:
+      data = np.c_[self.x, self.y]
+
+    np.savetxt(filename, data)
+
+
+
 def _ranges_to_index(x, ranges):
   """
   """
@@ -73,7 +102,10 @@ def processor(func):
   Function decorator that adds an identically named method to the Curve class.
 
   `func` must take a Curve as its first parameter, and should return a Curve
+
+  TODO: add ability to customize name
   """
+
   setattr(Curve, func.__name__, func)
   return func
 
@@ -154,6 +186,9 @@ def broaden(curve, sigma):
   gx = np.arange(-4*sigma, 4*sigma+dx, dx)
   gy = np.exp(-gx**2 / (2 * sigma**2)) / np.sqrt(2 * np.pi * sigma**2)
   new_y = np.convolve(curve.y,gy,'same') / gy.sum()
+
+  if len(gx) > len(curve.x):
+    new_y = np.interp(curve.x,gx,new_y)
 
   # XXX How should uncertainty be handled (stripped for now)?
   return Curve(curve.x, new_y)
@@ -317,10 +352,79 @@ def fit_voigt_bg(curve, fit_ranges=None, guess=(1.0,0.0,1.0, 0.1, 0, 0)):
   new_y = voigt_bg(fit, curve.x)
   return Curve(curve.x, new_y, curve.s, voigt_parameters=fit)
 
+def _savitzky_golay(y, window_size, order, deriv=0):
+    r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
+    The Savitzky-Golay filter removes high frequency noise from data.
+    It has the advantage of preserving the original shape and
+    features of the signal better than other types of filtering
+    approaches, such as moving averages techhniques.
+    Parameters
+    ----------
+    y : array_like, shape (N,)
+        the values of the time history of the signal.
+    window_size : int
+        the length of the window. Must be an odd integer number.
+    order : int
+        the order of the polynomial used in the filtering.
+        Must be less then `window_size` - 1.
+    deriv: int
+        the order of the derivative to compute (default = 0 means only smoothing)
+    Returns
+    -------
+    ys : ndarray, shape (N)
+        the smoothed signal (or it's n-th derivative).
+    Notes
+    -----
+    The Savitzky-Golay is a type of low-pass filter, particularly
+    suited for smoothing noisy data. The main idea behind this
+    approach is to make for each point a least-square fit with a
+    polynomial of high order over a odd-sized window centered at
+    the point.
+    Examples
+    --------
+    t = np.linspace(-4, 4, 500)
+    y = np.exp( -t**2 ) + np.random.normal(0, 0.05, t.shape)
+    ysg = savitzky_golay(y, window_size=31, order=4)
+    import matplotlib.pyplot as plt
+    plt.plot(t, y, label='Noisy signal')
+    plt.plot(t, np.exp(-t**2), 'k', lw=1.5, label='Original signal')
+    plt.plot(t, ysg, 'r', label='Filtered signal')
+    plt.legend()
+    plt.show()
+    References
+    ----------
+    .. [0] http://www.scipy.org/Cookbook/SavitzkyGolay
+    .. [1] A. Savitzky, M. J. E. Golay, Smoothing and Differentiation of
+       Data by Simplified Least Squares Procedures. Analytical
+       Chemistry, 1964, 36 (8), pp 1627-1639.
+    .. [2] Numerical Recipes 3rd Edition: The Art of Scientific Computing
+       W.H. Press, S.A. Teukolsky, W.T. Vetterling, B.P. Flannery
+       Cambridge University Press ISBN-13: 9780521880688
+    """
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError, msg:
+        raise ValueError("window_size and order have to be of type int")
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError("window_size size must be a positive odd number")
+    if window_size < order + 2:
+        raise TypeError("window_size is too small for the polynomials order")
+    order_range = range(order+1)
+    half_window = (window_size -1) // 2
+    # precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv]
+    # pad the signal at the extremes with
+    # values taken from the signal itself
+    firstvals = y[0] - np.abs( y[1:half_window+1][::-1] - y[0] )
+    lastvals = y[-1] + np.abs(y[-half_window-1:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve( m, y, mode='valid')
+
 @processor
-def savitzky_golay(curve, window_size, order):
-  from utils import savitzky_golay as sg
-  new_y = sg(curve.y, window_size, order)
+def savitzky_golay(curve, window_size, order, deriv=0):
+  new_y = _savitzky_golay(curve.y, window_size, order, deriv)
   return Curve(curve.x, new_y, curve.s)
 
 
@@ -395,3 +499,91 @@ def subtract(curve, curve2, interpolate=None, **kwargs):
     new_s == curve2.s
 
   return Curve(new_x, new_y, new_s)
+
+@processor
+def combine(curve, curve2, op, interpolate=None, **kwargs):
+  """
+  Combine y values of `curve` and `curve2` using `operator`.
+
+  Parameters:
+    curve - left side argument
+    curve2 - right side argument
+    op - operator used to combine y values
+    interpolate - None (default), 'linear', or 'spline'
+    kwargs - keyword args to pass on to the interpolation method
+             see `interpolate()` and `interpolate_spline()`
+
+  If curve2.x differs from curve.x, then curve2 is interpolated onto curve.x using the method specified by `interpolate`. If  `interpolate` is None, a ValueError is raised instead.
+  """
+
+  if (hasattr(curve2, 'x')):
+    if not np.all(curve.x == curve2.x):
+      if interpolate == 'linear' :
+        curve2 = curve2.interpolate(curve.x, **kwargs)
+      elif interpolate == 'spline':
+        curve2 = curve2.interpolate_spline(curve.x, **kwargs)
+      else:
+        raise ValueError("curve and curve2 must have same x values, or an interpolation method must be specificied")
+    y2 = curve2.y
+    s2 = curve2.s
+  else:
+    y2 = curve2
+    s2 = None
+
+  new_x = curve.x
+  new_y = op(curve.y, y2)
+
+  if curve.s is not None and s2 is not None:
+    # XXX this isn't correct for all operations...
+    new_s = np.sqrt(curve.s**2 +s2**2)
+  elif curve.s is not None:
+    new_s == curve.s
+  elif s2 is not None:
+    new_s == s2
+  else:
+    new_s = None
+
+  return Curve(new_x, new_y, new_s)
+
+# standard operations
+@processor
+def __add__(curve, curve2, interpolate='linear', **kwargs):
+  return combine(curve, curve2, operator.add, interpolate, **kwargs)
+
+@processor
+def __sub__(curve, curve2, interpolate='linear', **kwargs):
+  return combine(curve, curve2, operator.sub, interpolate, **kwargs)
+
+@processor
+def __mul__(curve, curve2, interpolate='linear', **kwargs):
+  return combine(curve, curve2, operator.mul, interpolate, **kwargs)
+
+@processor
+def __div__(curve, curve2, interpolate='linear', **kwargs):
+  return combine(curve, curve2, operator.div, interpolate, **kwargs)
+
+@processor
+def fsum(curve, q, N):
+  from .const import HBARC, MC2
+  norm = N * (HBARC**2) * q**2 / MC2
+  ret = curve.copy()
+  ret.y *=  norm / np.trapz(ret.x*ret.y, ret.x)
+  return ret
+
+
+##################
+# Additional API #
+##################
+
+def plot_curves(curves, styles=None):
+  if styles is None:
+    styles = [{}]
+  elif isinstance(styles, dict):
+    styles = [styles]
+  elif not isinstance(styles, list):
+    raise ValueError("Invalid styles")
+
+  styles *= len(curves) / len(styles) + 1
+  for c, s in zip(curves, styles):
+    c.plot(**s)
+
