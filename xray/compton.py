2422cd09a31fc2c87b2b423dc90df2e7dd5e7e16
Brian Mattern <rephorm@rephorm.com>
2012-11-01 11:16:36 -0700

    compton shift at a given angle

--- xray/compton.py
+++ xray/compton.py
@@ -26,3 +26,30 @@ def fermi_profile(pq, N, V):
 
   pf = fermi_momentum(N/float(V))
   return V / (2 * np.pi)**2 * (pf**2 - pq**2) * (pf >= np.abs(pq))
+
+
+
+def compton_shift(E, theta):
+    """
+    Calculate the Compton shift at a fixed scattering angle
+
+    Parameters:
+      E: fixed energy in eV
+      theta: angle in degrees"
+
+    Returns:
+      Compton shift in eV
+
+    For fixed incident energy experiment (e.g. laser-shock XRTS), the peak will be located at the scattered energy E - Ec.
+    For fixed scattered energy (e.g. LERIX), the peack will be located at incident energy E + Ec.
+    """
+
+    from xray.const import HARTREE
+    c = 137.035999074 # inverse fine structure from CODATA
+
+    E = E / HARTREE # convert to a.u.
+    theta = theta * np.pi / 180 # convert to radians
+    A = 1
+    B = 2*E*(1-np.cos(theta)) - 2*c**2
+    C = 2*E**2*(1-np.cos(theta))
+    return (-B - np.sqrt(B**2 - 4 * A * C)) / 2 * HARTREE
