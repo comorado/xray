ec90f7673640e37ce5a4ca56117bca397c0ab987
Brian Mattern <rephorm@rephorm.com>
2012-11-30 11:23:37 -0800

    add code to process feff input files

--- xray/feff.py
+++ xray/feff.py
@@ -187,6 +187,167 @@ def load_atomic_wf(filename):
   indices = [wf[0] == i for i in np.unique(wf[0])]
   return [(wf[2][i], wf[3][i] + 1j * wf[4][i]) for i in indices]
 
+
+#
+# FEFF Input code below
+#
+
+def rot_axis_angle(a, b):
+  """
+  Find rotation axis and angle that rotates vector a to vector b
+  """
+
+  c = np.cross(a,b)
+  norm = np.linalg.norm
+  theta = np.arcsin(norm(c) / (norm(a) * norm(b)))
+
+  return (c,theta)
+
+def rotation_matrix(axis, theta):
+  R = np.zeros((3,3))
+
+  u = axis / np.linalg.norm(axis)
+  c = np.cos(theta)
+  d = 1- c
+  s = np.sin(theta)
+
+  R[0,0] = c + u[0]**2 * d
+  R[0,1] = u[0] * u[1] * d - u[2] * s
+  R[0,2] = u[0] * u[2] * d + u[1] * s
+  R[1,0] = u[1] * u[0] * d + u[2] * s
+  R[1,1] = c + u[1]**2 * d
+  R[1,2] = u[1] * u[2] * d - u[0] * s
+  R[2,0] = u[2] * u[0] * d - u[1] * s
+  R[2,1] = u[2] * u[1] * d + u[0] * s
+  R[2,2] = c + u[2]**2 * d
+
+  return R
+
+class Atom(object):
+  def __init__(self, x, y, z, pot=0, tag='', r=0.0, n=0):
+    self.x = x
+    self.y = y
+    self.z = z
+    self.pot = pot
+    self.tag = tag
+    self.r = r
+    self.n = n
+
+  def __str__(self):
+    return " %10.5f %10.5f %10.5f  %3d  %10s %10.5f % 5d" % (self.x,self.y,self.z,self.pot,self.tag,self.r,self.n)
+
+  def __repr__(self):
+    return "xray.feff.Atom(%10.5f, %10.5f, %10.5f)" % (self.x, self.y, self.z)
+
+class InputFile(object):
+  def __init__(self, filename):
+    if filename:
+      self.load(filename)
+
+  def load(self, filename):
+    self.filename = filename
+
+    self.pre_atoms = []
+    self.post_atoms = []
+    self.atoms = []
+    self.cur = self.pre_atoms
+
+    with open(filename) as f:
+      in_atoms = False
+
+      for line in f:
+        if not in_atoms:
+          self.cur.append(line)
+          if line.strip().startswith('ATOMS'):
+            in_atoms = True
+
+        else:
+          sline = line.strip()
+          if sline.startswith('END'):
+            self.cur = self.post_atoms
+            self.cur.append(line)
+            in_atoms = False
+          elif sline[0] == '*':
+            self.cur.append(line)
+          else:
+            pieces = line.split()
+            self.atoms.append(Atom(
+                x = float(pieces[0]),
+                y = float(pieces[1]),
+                z = float(pieces[2]),
+                pot = int(pieces[3]),
+                tag = pieces[4],
+                r = float(pieces[5]),
+                n = int(pieces[6])
+                ))
+
+  def scale_atoms_uniformly(self, scale):
+    self.scale_atoms(scale, scale, scale)
+
+  def scale_atoms(self, scale_x, scale_y, scale_z):
+    for a in self.atoms:
+      a.x *= scale_x
+      a.y *= scale_y
+      a.z *= scale_z
+      a.r = np.sqrt(a.x**2 + a.y**2 + a.z**2)
+
+  def rotate_atoms_to(self, zhat):
+    """
+    Rotates atoms so that z axis now points along `zhat`
+    """
+    ax,ang = rot_axis_angle(np.array([0.,0.,1.]), zhat)
+    R = rotation_matrix(ax, -ang)
+    self.rotate_atoms(R)
+
+  def rotate_atoms(self, rotation_matrix):
+    for a in self.atoms:
+      a.x,a.y,a.z = np.dot(rotation_matrix, np.array([a.x,a.y,a.z]))
+
+  def save(self, filename):
+    with to_filehandle(filename, "w") as (f, fname):
+      for line in self.pre_atoms:
+        f.write(line)
+
+      for a in self.atoms:
+        f.write(str(a) + "\n")
+
+      for line in self.post_atoms:
+        f.write(line)
+
+def vasp_atoms(feff_input, bounds):
+  xmin,xmax = bounds[0]
+  ymin,ymax = bounds[1]
+  zmin,zmax = bounds[2]
+
+  dx = xmax - xmin
+  dy = ymax - ymin
+  dz = zmax - zmin
+
+  # limit to atoms within bounds
+  atoms_in_box = [a for a in feff_input.atoms if
+                    xmin <= a.x <= xmax and
+                    ymin <= a.y <= ymax and
+                    zmin <= a.z <= zmax]
+
+  # scale to box coordinates
+  vatoms = []
+  for a in atoms_in_box:
+    x = (a.x - xmin) / dx
+    y = (a.y - ymin) / dy
+    z = (a.z - zmin) / dz
+    vatoms.append((x,y,z))
+
+
+  print "Comment"
+  print "    1.000"
+  print "    %10.5f %10.5f %10.5f" % (dx,0,0)
+  print "    %10.5f %10.5f %10.5f" % (0,dy,0)
+  print "    %10.5f %10.5f %10.5f" % (0,0,dz)
+  print "Selective dynamics"
+  print "Direct"
+  for va in vatoms:
+    print "    %10.5f %10.5f %10.5f F F F" % va
+
 if __name__ == "__main__":
   import sys
 
@@ -208,3 +369,27 @@ if __name__ == "__main__":
     #print "N_check: ", N(mu, E, T, dos)
 
   #plot_ldos(T,mu)
+
+
+# XXX the following two functions should go elsewhere...
+def writable(fname):
+  return hasattr(fname, 'write')
+
+from contextlib import contextmanager
+@contextmanager
+def to_filehandle(fname_or_fh, flag="r"):
+  """
+  Either open a new file or use existing filehandle for IO
+  """
+  try:
+    if writable(fname_or_fh):
+      fh = fname_or_fh
+      own_fh = False
+      fname_or_fh = None
+    else:
+      fh = open(fname_or_fh, flag)
+      own_fh = True
+    yield fh, fname_or_fh
+  finally:
+    if own_fh:
+      fh.close()
