3dce86c9a123afd6ace64711e320234320152f28
Egor Klevak <comorado@gmail.com>
2015-03-02 19:52:15 -0800

    Implemened center of mass method for calculating center of mass velocity, need to test get frame routine addition which reads the file handler where it was left, for larger files it should make things easier.

--- xray/vasp.py
+++ xray/vasp.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 """
 This file contains tool box for working with XDATCAR file 
@@ -60,6 +61,9 @@ def readHeader(f):
       return (natoms, xvector, yvector, zvector)
 
 def countFrames(f):
+      """
+      Helper method counts number of frames in the file
+      """
       numberOfAtoms = readHeader(f)[0] 
       nextLine = True
       blankLines = 0
@@ -78,27 +82,116 @@ def countFrames(f):
       f.seek(0) 
       return int(frameCount)
 
-def readFrame(f, frameNumber):
+def readFrame(f, frameNumber = 1, fileName = None):
+      """
+      Helper method, reads specified frame from the file handler,
+      returns array of objects of atoms
+      """
       nextLine = True
       blankLines = 0
       totalLines = 0
       headerLines = 8
-      while(nextLine):
-         line = f.readline()
-         totalLines += 1
-         if line.strip() == '' and totalLines > headerLines and blankLines == frameNumber:
-            blankLines += 1
-            #while ():
+      l = 0
+      atoms = []
+      if (fileName == None):
+         numberOfAtoms, xvector, yvector, zvector  = readHeader(f) 
+   
+         while(nextLine):
+            line = f.readline()
+            totalLines += 1
+   
+            if (line.strip() == '' and totalLines > headerLines):
+               blankLines += 1
+   
+               while (blankLines == frameNumber):
+                  line = f.readline()
+   
+                  if line.strip() != '':
+                     pieces = line.split()
+                     atoms.append(Atom(
+                          x = float(pieces[l  ]) * xvector[0],
+                          y = float(pieces[l+1]) * yvector[1],
+                          z = float(pieces[l+2]) * zvector[2],
+                          r = 0))
+                  else:
+                     blankLines += 1        
+                     nextLine = False
+   
+         f.seek(0) 
+         assert (len(atoms) == numberOfAtoms), "Number of atoms is wrong " + str(len(atoms))
+         assert (len(atoms) != 0), "Frame number is out of bounds " + str(len(atoms))
+         
+         return atoms
 
-            atoms.append(AtomXYZ(
-                 x = float(pieces[l]) * self.xvector[0],
-                 y = float(pieces[l+1]) * self.yvector[1],
-                 z = float(pieces[l+2]) * self.zvector[2],
-                 r = 0))
+      else: 
+         fileHandler = open(fileName,'r') 
+         numberOfAtoms, xvector, yvector, zvector  = readHeader(fileHandler) 
+         while(nextLine):
+            line = f.readline()
+            totalLines += 1
+   
+            if (line.strip() == ''):
+               blankLines += 1
+   
+               while (blankLines == 1):
+                  line = f.readline()
+   
+                  if (line.strip() != ''):
+                     pieces = line.split()
+                     atoms.append(Atom(
+                          x = float(pieces[l  ]) * xvector[0],
+                          y = float(pieces[l+1]) * yvector[1],
+                          z = float(pieces[l+2]) * zvector[2],
+                          r = 0))
+                  else:
+                     blankLines += 1        
+   
+         assert (len(atoms) == numberOfAtoms), "Number of atoms is wrong " + str(len(atoms))
+         assert (len(atoms) != 0), "Frame number is out of bounds " + str(len(atoms))
+         
+         return atoms
 
-         if (line == ""):
-            nextLine = False
+def comV(f, frameMax = None):
+    """
+    Calculates center of mass velocity by going through frames
+    """
+    previousFrame = readFrame(f)	
+    if frameMax == None:
+       nframe = countFrames(f)
+    else:
+       nframe = frameMax
+    
+    for i in xrange(2, nframe-1):
+        currentFrame = readFrame(f, i)
+        print i, "\t".join(map(str,comVFrame(f, currentFrame, previousFrame)))
+        previousFrame = currentFrame
 
-      f.seek(0) 
-      return atoms
+def comVFrame(f, currentFrame, previousFrame):
+   """
+   Helper method for calculating velocity of COM between two adjecent frames
+   Checks if there is a translation by the size of unit cell
+   """
+   comVelocity = []
+   numberOfAtoms, xvector, yvector, zvector  = readHeader(f) 
+   #print readHeader(f)
+   dvx = 0
+   dvy = 0
+   dvz = 0
+   for i in xrange(0, int(numberOfAtoms)):
+        dx = currentFrame[i].x-previousFrame[i].x
+        dy = currentFrame[i].y-previousFrame[i].y
+        dz = currentFrame[i].z-previousFrame[i].z
 
+        if abs(dx)+0.1 >= xvector[0]:
+           dx -= math.copysign(xvector[0],dx)
+        if abs(dy)+0.1 >= yvector[1]:
+           dy -= math.copysign(yvector[1],dy)
+        if abs(dz)+0.3 >= zvector[2]:
+           dz -= math.copysign(zvector[2],dz)
+        
+        dvx += dx
+        dvy += dy
+        dvz += dz
+
+   return (dvx/numberOfAtoms,dvy/numberOfAtoms,dvz/numberOfAtoms)
+   
