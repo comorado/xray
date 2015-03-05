c34429a8ad00e3562e4ee86ee78933975623d709
Egor Klevak <comorado@gmail.com>
2015-03-05 15:28:59 -0800

    Modified readFrame routine,now I can read in Frames with non empty line separator between them.

--- xray/vasp.py
+++ xray/vasp.py
@@ -28,7 +28,6 @@ def readHeader(f):
       Reads the header information form the XDATCAR file
       Leaves file handler at the position past the header
       """
-      #f = open(filename,"r")
       nextLine = True
       scale   = 1.0
       natoms  = 0
@@ -73,7 +72,7 @@ def countFrames(f):
       while(nextLine):
          line = f.readline()
          totalLines += 1
-         if line.strip() == '':
+         if line.strip() == '' or line.find('Direct') != -1:
             blankLines += 1
             frameCount = float(totalLines - blankLines) / numberOfAtoms  
             assert (frameCount.is_integer()), "Not consistent number of entries in frame "+ str(blankLines)
@@ -89,9 +88,12 @@ def readFrame(f):
       """
       l = 0
       atoms = []
-   
       line = f.readline()
-      while(line.strip() != ''):
+
+      while(line.find('Direct') == -1 and line.strip() != ''):
+            if (line.find('Direct') != -1):
+                print line.find('Direct') != -1
+
             pieces = line.split()
             atoms.append(Atom(
               x = float(pieces[l  ]),
@@ -112,13 +114,15 @@ def comV(f, frameMax = None):
        nframe = frameMax
     
     numberOfAtoms, xvector, yvector, zvector  = readHeader(f) 
-    print numberOfAtoms, xvector, yvector, zvector 
+    #print numberOfAtoms, xvector, yvector, zvector 
     previousFrame = readFrame(f)	
 
-    print len(previousFrame)
+    #print len(previousFrame)
     for i in xrange(2, nframe-1):
         currentFrame = readFrame(f)
-        print i, "\t".join(map(str,comVFrame(f, currentFrame, previousFrame,numberOfAtoms, xvector, yvector, zvector)))
+        #print i, "\t".join(map(str,comVFrame(f, currentFrame, previousFrame,numberOfAtoms, xvector, yvector, zvector)))
+        xv, yv, zv = comVFrame(f, currentFrame, previousFrame,numberOfAtoms, xvector, yvector, zvector)
+        print " %04d\t%10.5f\t%10.5f\t%10.5f\t%10.5f" % (i, xv, yv, zv,(xv**2+yv**2+zv**2)**(0.5))
         previousFrame = currentFrame
 
 def comVFrame(f, currentFrame, previousFrame, numberOfAtoms, xvector, yvector, zvector):
@@ -130,16 +134,17 @@ def comVFrame(f, currentFrame, previousFrame, numberOfAtoms, xvector, yvector, z
    dvx = 0
    dvy = 0
    dvz = 0
+   tol = 0.1
    for i in xrange(0, int(numberOfAtoms)):
         dx = (currentFrame[i].x-previousFrame[i].x) * xvector[0]
         dy = (currentFrame[i].y-previousFrame[i].y) * yvector[1]
         dz = (currentFrame[i].z-previousFrame[i].z) * zvector[2]
 
-        if abs(dx)+0.1 >= xvector[0]:
+        if abs(dx) + tol >= xvector[0]:
            dx -= math.copysign(xvector[0],dx)
-        if abs(dy)+0.1 >= yvector[1]:
+        if abs(dy) + tol >= yvector[1]:
            dy -= math.copysign(yvector[1],dy)
-        if abs(dz)+0.3 >= zvector[2]:
+        if abs(dz) + tol >= zvector[2]:
            dz -= math.copysign(zvector[2],dz)
         
         dvx += dx
@@ -147,4 +152,50 @@ def comVFrame(f, currentFrame, previousFrame, numberOfAtoms, xvector, yvector, z
         dvz += dz
 
    return (dvx/numberOfAtoms,dvy/numberOfAtoms,dvz/numberOfAtoms)
-   
+
+def pairDistance(atoms, numberOfAtoms, xvector, yvector, zvector, cutoff=None, xyz=None):
+    """
+    Helper method for calculating pair distance in provided array atoms coordinates
+    Uses cutoff distance to optimize calculation, if not specified sets to 4 angs 
+    """
+    #print cutoff
+    if cutoff == None:
+       cutoff = min([xvector[0],yvector[1],zvector[2]])
+       if cutoff > 5:
+          cutoff = 4
+
+    dist = []
+    m = 0
+    d = 0
+    if xyz==None:
+      for atom1 in atoms:
+         for atom2 in atoms:
+            for ix in range(-1,2):
+              dx = (atom1.x - (ix * xvector[0] + atom2.x))**2
+              if (dx**(0.5) < cutoff):
+                for iy in range(-1,2):
+                  dy = dx + (atom1.y - (iy * yvector[1] + atom2.y))**2
+                  if (dy**(0.5) < cutoff):
+                    for iz in range(-1,2):
+                      d =(dy +(atom1.z - (iz * zvector[2] + atom2.z))**2)**(0.5)
+  
+                      if d != 0 and d < cutoff:
+                        dist.append(d)
+                        m += 1
+
+    else:
+       for atom1 in atoms:
+         for atom2 in atoms:
+                  d = 0.0
+                  d += (atom1.x - atom2.x)**2
+                  d += (atom1.y - atom2.y)**2
+                  d += (atom1.z - atom2.z)**2
+
+                  d = d **(0.5)
+  
+                  if d != 0 and d < cutoff:
+                    dist.append(d)
+                    m += 1
+       
+    return dist
+  
